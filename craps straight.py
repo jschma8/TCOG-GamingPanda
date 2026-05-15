170e3fae456c0041b6e04857428cb1263dd558c6
jschma8 <email@email.com>
2026-05-15 12:49:46 -0400

    create straight through code building file

---
+++ craps straight.py
@@ -0,0 +1,20 @@
+import random
+
+LAYOUT = 'standard'
+COME = 'yes'
+POINT = 'off'
+WINNER = 'no'
+BETMARKER = 'off'
+
+def betStatus():
+    global COME
+    global POINT
+    global WINNER
+    global BETMARKER
+
+roll = random.randint(2,12)
+
+bet = 'pass'
+
+
+    
