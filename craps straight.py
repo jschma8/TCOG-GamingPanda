42c8ba200e828566eac780b260d66640f4c4ab00
jschma8 <email@email.com>
2026-05-18 08:43:44 -0400

    add roll repeater for straight run

--- craps straight.py
+++ craps straight.py
@@ -1,20 +1,34 @@
 import random
 
-LAYOUT = 'standard'
-COME = 'yes'
-POINT = 'off'
-WINNER = 'no'
-BETMARKER = 'off'
 
-def betStatus():
-    global COME
-    global POINT
-    global WINNER
-    global BETMARKER
-
-roll = random.randint(2,12)
-
-bet = 'pass'
+def main(i):
+    LAYOUT = 'standard'
+    POINT = 'off'
+    WINNER = 'no'
+    BETMARKER = 'off'
 
 
-    
+    roll = random.randint(2,12)
+
+    # bet = input('(P)ass or (NP)o Pass: ')
+    # bet = str.lower(bet)
+
+    if roll in (2,3,12):
+        POINT = 'craps'
+        WINNER = 'loss'
+
+    elif roll in (7, 11):
+        WINNER = 'yes'
+    else:
+        POINT = roll
+    print(f'# is {i}')
+    print(roll)
+    print(LAYOUT)
+    print(POINT)
+    print(WINNER)
+    print(BETMARKER)
+    print('')
+
+if __name__ == '__main__':
+    for i in range(6):
+        main(i)
